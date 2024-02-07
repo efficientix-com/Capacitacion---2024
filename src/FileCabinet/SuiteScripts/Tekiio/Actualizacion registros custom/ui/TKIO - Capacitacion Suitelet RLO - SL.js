6455c517e57f3b5d33f67b7b1bf8243c2328d017
Ricardo <ricardo.lopez@freebug.mx>
2024-02-07 12:43:16 -0600

    first commit

---
+++ src/FileCabinet/SuiteScripts/Tekiio/Actualizacion registros custom/ui/TKIO - Capacitacion Suitelet RLO - SL.js
@@ -0,0 +1,36 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/log', 'N/record', 'N/search', 'N/ui/serverWidget'],
+    /**
+     * @param{log} log
+     * @param{record} record
+     * @param{search} search
+     */
+     /**
+     * @name TKIO - Capacitacion Suitelet RLO - SL
+     * @version 1.0
+     * @author Ricardo López <ricardo.lopez@freebug.mx>
+     * @summary Script de interfaz pruebas
+     * @copyright Tekiio México 2022
+     * 
+     * Cliente       -> Development
+     * Last modification  -> 07/02/2024
+     * Modified by     -> Ricardo López <ricardo.lopez@freebug.mx>
+     * Script in NS    -> Registro en Netsuite <_tkio_capacitacion_suilet_rlo_sl>
+     */
+    (log, record, search, serverWidget) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+        }
+
+        return { onRequest }
+
+    });
